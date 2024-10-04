bc6f6e2356982b4e1bfd6e052c01ec97afb766c8
Serj910vip <56538196+Serj910vip@users.noreply.github.com>
2024-10-04 16:59:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,30 +8,35 @@ app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        # получаем выбранное изображение
+        # Получаем выбранное изображение
         selected_image = request.form.get('image-selector')
 
-        # Задание №2.Получаем текст
-        text_top=request.form.get('textTop')
-        #text_bottom = 
+        # Получаем текст
+        text_top = request.form.get('textTop')
+        text_bottom = request.form.get('textBottom')
 
-        # Задание №3. Получаем расположение текста
-       
-
-        # Задание №3. Получаем цвет текста
+        # Получаем расположение текста
+        text_top_y = request.form.get('textTop_y')
+        text_bottom_y = request.form.get('textBottom_y')
+        
+        # Получаем цвет текста
         selected_color = request.form.get('color-selector')
-
-        return render_template('index.html', 
-                               # отображаем выбранное изображение
-                               selected_image=selected_image, 
-
-                               # Задание №2. Отображаем текст
-                               text_top = text_top,
-
-                               # Задание №3. Отображаем цвет 
-                               selected_color = selected_color
+        
+        return render_template('index.html',
                                
-                               #Задание №3. Отоброжаем расположение текста
+                               # Отображаем выбранное изображение
+                               selected_image=selected_image,
+
+                               # Отображаем текст
+                               text_top = text_top,
+                               text_bottom = text_bottom,
+
+                               # Отображаем цвет 
+                               selected_color = selected_color,
+                               
+                               # Отоброжаем расположение текста
+                               text_top_y = text_top_y,
+                               text_bottom_y = text_bottom_y
 
                                )
     else:
